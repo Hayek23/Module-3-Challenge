c9bbf255d7a422830db604a42651ee16e2c417e0
jwm <jwm3926@gmail.com>
2022-09-19 22:02:24 -0400

    fixed js so password will show up and not say undefined, cleaned up code alot

--- Develop/script.js
+++ Develop/script.js
@@ -10,29 +10,96 @@ function writePassword() {
 
 }
 
+function generate(length, characters){
+  var password = '';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    password += characters.charAt(Math.floor(Math.random() *
+    charactersLength))
+  }
+  return password
+}
+
 function generatePassword() {
-  
   var userLength = window.prompt("what is the minimum amunt of charaters you would like? (8-126)")
   var userLowerCase = window.confirm("Would you like lowercase letters?");
   var userUpperCase = window.confirm("Would you like uppercase letters?");
   var userNumbers = window.confirm("Would you like numerical characters?");
   var userSpecialCharacters = window.confirm("Would you like special characters?");
 if (userLowerCase & !userUpperCase & !userNumbers & !userSpecialCharacters){
-  function generate(length) {
-    var result           = '';
-    var characters       = 'abcdefghijklmnopqrstuvwxyz';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * 
- charactersLength));
-   }
-   return result;
-}
+
+ var password= generate(userLength, "abcdefghijklmnopqrstuvwxyz");
+  return password;
+
+} else if (!userLowerCase & userUpperCase & !userNumbers & !userSpecialCharacters){
+
+  var password = generate(userLength, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+  return password;
+
+} else if (!userLowerCase & !userUpperCase & userNumbers & !userSpecialCharacters){
+
+  var password = generate(userLength, "1234567890");
+  return password;
+
+} else if (!userLowerCase & !userUpperCase & !userNumbers & userSpecialCharacters){
+
+  var password = generate(userLength, "!@#$%^&*()_+=-.,");
+  return password;
+
+} else if (userLowerCase & userUpperCase & !userNumbers & !userSpecialCharacters){
+
+  var password = generate(userLength, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+  return password;
+
+} else if (!userLowerCase & userUpperCase & userNumbers & !userSpecialCharacters){
+
+  var password = generate(userLength, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
+  return password;
+
+} else if (userLowerCase & userUpperCase & userNumbers & userSpecialCharacters){
+
+  var password = generate(userLength, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+");
+  return password;
+
+} else if (!userLowerCase & userUpperCase & userNumbers & !userSpecialCharacters){
+
+  var password = generate(userLength, "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
+  return password;
+
+} else if (!userLowerCase & userUpperCase & userNumbers & userSpecialCharacters){
+
+  var password = generate(userLength, "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
+  return password;
+
+} else if (!userLowerCase & userUpperCase & !userNumbers & userSpecialCharacters){
+
+  var password = generate(userLength, "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$^%&*()_+");
+  return password;
+
+} else if (!userLowerCase & !userUpperCase & userNumbers & userSpecialCharacters){
+
+  var password = generate(userLength, "1234567890!@#$%^&*()_+");
+  return password;
+
+} else if (userLowerCase & !userUpperCase & !userNumbers & userSpecialCharacters){
+
+  var password = generate(userLength, "abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+");
+  return password;
+
+} else if (userLowerCase & !userUpperCase & userNumbers & !userSpecialCharacters){
+  
+  var password = generate(userLength, "abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+");
+  return password;
+
+} else if (userLowerCase & userUpperCase & userNumbers & !userSpecialCharacters){
+
+  var password = generate(userLength, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
 
 console.log(generate(userLength));
-} else if (userLowerCase & userUpperCase & userNumbers & userSpecialCharacters)
 
 }
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
